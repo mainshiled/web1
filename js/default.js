2174457ef82f0cff57e5df635442ade3d9b4397f
mainshiled <yanakikouta1997@gmail.com>
2017-07-24 17:05:47 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -36,7 +36,10 @@ if(last_date){
   function getFileName(){
  return window.location.href.split('/').pop();
  }
+   
   var thmubs =document.querySelectorAll('.thumb');
    for(idx in thmubs){
     thmubs[idx].onclick=function(){
      document.getElementById("bigimg").src = this.dataset.image+'.png';
+    }
+   }
